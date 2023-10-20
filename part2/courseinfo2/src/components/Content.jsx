abcd29d23b3dc7447b55811d90e2d698baff755b
NumskullLee <ihe330604419@gmail.com>
2023-10-20 14:36:44 -0600

    2.3 Courseinfo step 7 & 8

--- part2/courseinfo2/src/components/Content.jsx
+++ part2/courseinfo2/src/components/Content.jsx
@@ -1,9 +1,15 @@
 import Part from "./Part"
 
+
+
 const Contetn = (props)=>{
+  const array=props.parts.map(data=>data.exercises);  
+  const total = array.reduce((s, p) => s + p);
+
   return(
     <>
       {props.parts.map(part=> <Part part={part} key={part.id}/> )}
+      <p>Total of {total} exercises </p>
     </>
   )
 }
