3eb4603c1fa22f37e3cf5d61b42dcbbd5baf9bf4
NumskullLee <ihe330604419@gmail.com>
2023-10-24 10:44:36 -0600

    Part 2 phonebook step 4

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -4,17 +4,27 @@ import { useState } from 'react'
 const App = () => {
  
   const [persons, setPersons] = useState([
-    {name:'Arto Hellas'}
-  ]);
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+
   const [newName, setNewName] = useState('');
   const [newPhone, setNewPhone] = useState('');
+  const [filter, setNewFilter] = useState('');
 
   const inputEventName=(event)=>{
-    setNewName(event.target.value)
+    setNewName(event.target.value);
   }
 
   const inputEventPhone=(event)=>{
-    setNewPhone(event.target.value)
+    setNewPhone(event.target.value);
+  }
+
+  const inputEventFilter=(event)=>{
+    setNewFilter(event.target.value);
+    
   }
 
   const addNewName=(event)=>{
@@ -29,17 +39,27 @@ const App = () => {
     }else{
     const newObj = {
         name:newName,
-        number:newPhone
+        number:newPhone,
+        id: persons.length+1
     };
     setPersons(persons.concat(newObj));
     setNewName('');
     setNewPhone('');
+    console.log(persons)
     }
   }
+
+
+
+
   return (
     <>
       <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input onChange={inputEventFilter} value={filter}/>
+      </div>
+      <h3>Add new</h3>
       <form onSubmit={addNewName}>
           <div>
             name: <input onChange={inputEventName} value={newName}/>
@@ -52,9 +72,9 @@ const App = () => {
           </div>
       </form>
       </div>
-      <h2>Numbers</h2>
+      <h3>Numbers</h3>
       <ul>
-        {persons.map(person=><li key={person.name}>{person.name} {person.number}</li>)}
+        {persons.filter(person=> person.name.includes(filter) || person.number.includes(filter)).map(person=><li key={person.id}>{person.name} {person.number}</li>)}
       </ul>
     </>
   )
